aac01ecd36b145f210ba72ab800638d900fea6c8
Carsten Seeger <c.seeger@epages.com>
2019-07-01 09:48:01 +0200

    added doc.go

---
+++ lib/doc.go
@@ -0,0 +1,16 @@
+/*
+
+Mail2Most is an implementation for reading and filtering emails and pushing them to mattermost
+
+It uses the IMAP protocol to connect to an email account and can filter via:
+
+- Folder
+- Subject
+- From
+- To
+- Time range
+
+and pushes the subject and body into mattermost
+
+*/
+package main
