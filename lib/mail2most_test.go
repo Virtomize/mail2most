9cc77c2543249fc0c616dcba6906e3efee60881a
Carsten Seeger <c.seeger@epages.com>
2019-08-13 11:23:16 +0200

    #9 removed initial start test since it does not error anymore
    Signed-off-by: Carsten Seeger <c.seeger@epages.com>

--- lib/mail2most_test.go
+++ lib/mail2most_test.go
@@ -11,9 +11,6 @@ func TestRun(t *testing.T) {
 	m2m, err := New("../conf/mail2most.conf")
 	assert.Nil(t, err)
 
-	err = m2m.Run()
-	assert.NotNil(t, err)
-
 	_, err = os.Create("/tmp/data.json")
 	assert.Nil(t, err)
 
